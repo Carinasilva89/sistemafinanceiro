0dd7d67e2d35bd2812a75d555819730e0147dc51
rayaneferreira <bd.rayaneferreira@gmail.com>
2023-05-08 20:30:54 -0300

    first commit

---
+++ Usuario.cs
@@ -0,0 +1,52 @@
+public class Usuario{
+
+private string? nome;
+private string? cpf;
+private string? email;
+private int? id;
+
+public Usuario(){
+
+}
+public Usuario(string nome, string cpf, string email, int id){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.id = id;
+}
+
+public string getNome(){
+    return this.nome;
+}
+public void setNome(string nome){
+    this.nome = nome;
+}
+
+public string getCpf(){
+    return this.cpf;
+}
+public void setCpf(string cpf){
+    this.cpf = cpf;
+}
+
+public string getEmail(){
+    return this.email;
+}
+public void setEmail(string email){
+    this.email = email;
+}
+
+public int getId(){
+    return (int) this.id;
+}
+public void setId(int id){
+    this.id = id;
+}
+
+
+
+
+
+
+
+}
